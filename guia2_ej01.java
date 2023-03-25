6854acd1d37244b4669d301c0bb02cc41fb2d3cf
Paula Batalla <paupabatalla@gmail.com>
2023-03-25 19:50:33 -0300

    Primer commit

---
+++ guia2_ej01.java
@@ -0,0 +1,13 @@
+package ejercicios;
+import java.util.Scanner;
+/*Consigna
+ */
+public class guia2_ej01 {
+    public static void main(String[] args) {
+    
+       Scanner leer = new Scanner(System.in);
+       
+       
+    }
+
+}
